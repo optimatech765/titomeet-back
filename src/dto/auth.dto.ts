1957c4bde86bdf196f52ea1b16ec1a2e1fd3da47
optimatech765 <optimatech88@gmail.com>
2025-02-18 00:54:04 +0100

    setup swagger api doc

---
+++ src/dto/auth.dto.ts
@@ -0,0 +1,21 @@
+export class CreateUserDto {
+  @ApiProperty()
+  @IsString()
+  @IsEmail()
+  email: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  firstName: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  lastName: string;
+}
