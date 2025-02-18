ea1829d5bd2063c5084feca401780badde852a52
optimatech765 <optimatech88@gmail.com>
2025-02-18 01:54:01 +0100

    config utils

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,28 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { CacheModule } from '@tenbou/test-shared-lib';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env', // Ensure this path is correct
+    }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 1000,
+        limit: 20,
+      },
+    ]),
+    CacheModule,
+    EventEmitterModule.forRoot(),
+    ScheduleModule.forRoot(),
+    AuthModule
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
-
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { SignupDto } from '../dto/auth.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { User } from '@tenbou/test-shared-lib';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private prisma: PrismaService) {}
+
+    async signup(signupDto: SignupDto) : Promise<User> {
+        // Check if user already exists
+        const user = await this.prisma.user.findUnique({
+            where: { email: signupDto.email }
+        })
+        if (user) {
+            throw new HttpException('User already exists', HttpStatus.BAD_REQUEST)
+        }
+        // Create user
+        const newUser = await this.prisma.user.create({
+            data: signupDto
+        })
+        return newUser
+}
