211be5d979cea4111b8d478067c7358860feb7a0
optimatech765 <optimatech88@gmail.com>
2025-03-17 09:47:43 +0100

    fix cors setup

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import helmet from 'helmet';
 config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
 
