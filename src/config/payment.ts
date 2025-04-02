9d534eb14a7f1eb49fa9f724aac6162e75d86ba2
optimatech765 <optimatech88@gmail.com>
2025-04-02 11:52:05 +0100

    update api url

--- src/config/payment.ts
+++ src/config/payment.ts
@@ -2,7 +2,10 @@ const paymentConfig = () => ({
   fedapay: {
     secretKey: process.env.FEDAPAY_SECRET_KEY,
     environment: process.env.FEDAPAY_ENVIRONMENT,
-    apiUrl: process.env.FEDAPAY_API_URL,
+    apiUrl:
+      process.env.FEDAPAY_ENVIRONMENT === 'sandbox'
+        ? 'https://sandbox-api.fedapay.com'
+        : 'https://api.fedapay.com',
   },
 });
 
