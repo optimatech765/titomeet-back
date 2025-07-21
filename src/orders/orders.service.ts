40001514b4c902e27184d6f40aecc356e57901d8
Gael HOUNGBEDJI <you@example.com>
2025-07-21 16:13:22 +0200

    get orders by user

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -2,7 +2,8 @@ import {
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
-import { PrismaService } from '@optimatech88/titomeet-shared-lib';
+import { getPaginationData, PrismaService, User } from '@optimatech88/titomeet-shared-lib';
+import { GetEventOrdersQueryDto } from 'src/dto/events.dto';
 import { throwServerError } from 'src/utils';
 
 @Injectable()
@@ -33,6 +34,47 @@ export class OrdersService {
         );
       } */
       return order;
+    } catch (error) {
+      return throwServerError(error);
+    }
+  }
+
+  async getOrdersByUser(query: GetEventOrdersQueryDto, user: User) {
+    try {
+      const { page, limit } = getPaginationData(query);
+      const where = {
+        userId: user.id,
+      }
+      const orders = await this.prisma.order.findMany({
+        where,
+        include: {
+          items: {
+            select: {
+              eventPriceId: true,
+              quantity: true,
+              unitPrice: true,
+            },
+          },
+          event: true
+        },
+        skip: (page - 1) * limit,
+        take: limit,
+        orderBy: {
+          createdAt: 'desc',
+        },
+      });
+
+      const total = await this.prisma.order.count({
+        where,
+      });
+
+      return {
+        items: orders,
+        total,
+        page,
+        limit,
+        totalPages: Math.ceil(total / limit),
+      };
     } catch (error) {
       return throwServerError(error);
     }
